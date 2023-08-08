3444ae4f2b2da0dd1003c1fa9abdd2da9e77dbc5
i.ismailov <ilyas.main@gmail.com>
2023-08-08 15:30:19 +0600

    SPRT-1035 review: moved business logic to service

--- tsapp/src/main/java/dev/ibreal/notification/controller/ProjectSettingController.java
+++ tsapp/src/main/java/dev/ibreal/notification/controller/ProjectSettingController.java
@@ -1,40 +1,35 @@
 package dev.ibreal.notification.controller;
 
 import dev.ibreal.notification.dto.ProjectSettingDTO;
-import dev.ibreal.notification.model.Project;
 import dev.ibreal.notification.model.ProjectSetting;
-import dev.ibreal.notification.repository.ProjectRepository;
 import dev.ibreal.notification.repository.ProjectSettingRepository;
+import dev.ibreal.notification.service.ProjectSettingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
-
 import static dev.ibreal.notification.controller.ProjectSettingController.PROJECT_SETTING_CONTROLLER_PATH;
 
 @RestController
 @RequestMapping("${base-url}" + PROJECT_SETTING_CONTROLLER_PATH)
 @RequiredArgsConstructor
 public class ProjectSettingController {
+
     public static final String PROJECT_SETTING_CONTROLLER_PATH = "/project/settings";
-    private final ProjectRepository projectRepository;
     private final ProjectSettingRepository projectSettingRepository;
+    private final ProjectSettingService projectSettingService;
+
     @PostMapping
     public ProjectSetting createProjectSetting(@RequestBody ProjectSettingDTO projectSettingDTO) {
-        Project project = projectRepository.findById(projectSettingDTO.getProjectId()).orElseThrow();
-        ProjectSetting projectSetting = new ProjectSetting();
-        projectSetting.setProject(project);
-        projectSetting.setNotificationType(projectSettingDTO.getNotificationType());
-        projectSetting.setTelegramChatId(projectSettingDTO.getGroupChatId());
-        return projectSettingRepository.save(projectSetting);
+        return projectSettingService.createProjectSetting(projectSettingDTO);
     }
 
     @GetMapping
     public List<ProjectSetting> getProjectsSettings() {
         return projectSettingRepository.findAll();
     }
+
 }
---
+++ tsapp/src/main/java/dev/ibreal/notification/service/ProjectSettingServiceImpl.java
@@ -0,0 +1,25 @@
+package dev.ibreal.notification.service;
+
+import dev.ibreal.notification.dto.ProjectSettingDTO;
+import dev.ibreal.notification.model.Project;
+import dev.ibreal.notification.model.ProjectSetting;
+import dev.ibreal.notification.repository.ProjectRepository;
+import dev.ibreal.notification.repository.ProjectSettingRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class ProjectSettingServiceImpl implements ProjectSettingService {
+    private final ProjectRepository projectRepository;
+    private final ProjectSettingRepository projectSettingRepository;
+
+    @Override
+    public ProjectSetting createProjectSetting(ProjectSettingDTO projectSettingDTO) {
+        Project project = projectRepository.findById(projectSettingDTO.getProjectId()).orElseThrow();
+        ProjectSetting projectSetting = new ProjectSetting();
+        projectSetting.setProject(project);
+        projectSetting.setNotificationType(projectSettingDTO.getNotificationType());
+        projectSetting.setTelegramChatId(projectSettingDTO.getGroupChatId());
+        return projectSettingRepository.save(projectSetting);
+    }
+
+}
